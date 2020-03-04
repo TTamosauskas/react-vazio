9d035fcdc85674b86412cee3af3d82acbbad42af
TTamosauskas <thiago.gt@gmail.com>
2020-03-04 12:06:20 -0300

    flex resets para tags html5

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,14 @@ import './styles.css';
 
 function Main() {
   return (
-<main role="main"><img width="40vmin" src="https://cdn.worldvectorlogo.com/logos/react.svg" alt="logo" /></main>
+<main role="main">
+  <div>
+    <img 
+    className="animated heartBeat infinite slower"
+    width="100vmin" 
+    src="https://cdn.worldvectorlogo.com/logos/react.svg" alt="logo" />
+  </div>
+  </main>
   );
 }
 
